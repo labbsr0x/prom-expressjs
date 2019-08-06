16d1d243ae5feae059e3281ce9ced8af47feb017
Abilio Esteves <abilio.esteves@ibm.com>
2019-08-06 17:08:35 -0300

    improved documentation and removed unnecessary dependency

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # express-prom
 Small repo with a full example of how to use NodeJS prom-client with ExpressJS
+
+# run
+Type on terminal to run:
+
+```
+> npm i
+> npm start
+```
+
+Then go to any random path on port `23498`, e.g. `http://localhost:23498/myname`.
+
+The request will be registered by Prometheus and can be viewed at `http://localhost:23498/metrics`.
--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const express = require("express");
+const express = require("express"); 
 const promclient = require("prom-client");
 
-const app = express();
+const app = express(); 
+
+// a cumulative histogram to collect http request metrics in well-defined buckets of interest
 const hist = new promclient.Histogram({
     name: "requests_seconds",
-    help: "measure the number of requests processed and its duration in seconds",
+    help: "measure the number of requests processed and its duration in seconds separated by well-defined buckets of interest",
     buckets: [0.3, 1, 2, 5],
     labelNames: [ "status", "method", "url" ]
 });
 
+// middleware to capture prometheus metrics for the request
 app.all(/^(?!\/metrics$).*/, (req, res, next) => {
     let end = hist.startTimer()
     next();
@@ -17,14 +20,17 @@ app.all(/^(?!\/metrics$).*/, (req, res, next) => {
     });
 })
 
+// endpoint to collect all the registered metrics
 app.get("/metrics", (req, res) => {
     res.status(200).header("Content-Type", "text/plain").send(promclient.register.metrics())
 });
 
+// a generic endpoint to simulate a valid operation
 app.all(/^(?!\/metrics$).*/, (req, res) => {
     setTimeout(() => {
         res.status(200).header("Content-Type", "text/plain").send(JSON.stringify({"success":true}));
-    }, 5000);
+    }, 4000);
 })
 
+// runs the express app on port 23498
 app.listen(23498)
